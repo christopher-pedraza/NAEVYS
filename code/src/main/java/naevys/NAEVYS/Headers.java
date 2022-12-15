6200a4c675daa7dfab874fd8d538615a242caf3f
Christopher Pedraza <gabalixreborn@gmail.com>
2022-12-15 16:11:22 -0600

    Agregar atributos a clase Headers

--- code/src/main/java/naevys/NAEVYS/Headers.java
+++ code/src/main/java/naevys/NAEVYS/Headers.java
@@ -1,18 +1,37 @@
 package naevys.NAEVYS;
 
+/**
+ * Clase utilizada para guardar los datos de las columnas que seran leidas del
+ * archivo fuente al igual que las columnas que se generaran en el archivo de
+ * salida
+ */
 public class Headers {
 	private String colName;
-	// Indice de la columna (el programa checara si corresponde a la columna con el
-	// nombre descrito en colName, sino, iterara hasta buscar la columna correcta)
+	// TODO: Indice de la columna (el programa checara si corresponde a la columna
+	// con el nombre descrito en colName, sino, iterara hasta buscar la columna
+	// correcta)
 	private int colIndex;
-
+	private char id;
+	private String value;
+	
 	/**
-	 * @param colName
-	 * @param colIndex
+	 * @param colName nombre de la columna
+	 * @param colIndex indice donde se deberia encontra la columna con el nombre colName
+	 * @param id identificador sobre el tipo de columna de la que se trata
+	 * @param value valor o referencia que se colocara en la columna
 	 */
-	public Headers(String colName, int colIndex) {
+	public Headers(String colName, int colIndex, char id, String value) {
 		this.colName = colName;
 		this.colIndex = colIndex;
+		this.id = id;
+		this.value = value;
+	}
+
+	/**
+	 * @return the colIndex
+	 */
+	public int getColIndex() {
+		return colIndex;
 	}
 
 	/**
@@ -23,10 +42,17 @@ public class Headers {
 	}
 
 	/**
-	 * @return the colIndex
+	 * @return the id
 	 */
-	public int getColIndex() {
-		return colIndex;
+	public char getId() {
+		return id;
+	}
+	
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
 	}
 
 	/**
