6dd7ce1faf7b86879fa2eeb1b169d1d4e75ab5c3
Christopher Pedraza <gabalixreborn@gmail.com>
2022-12-16 08:56:16 -0600

    Agregar libreria para leer Excel y funcion basica de lectura

--- code/src/main/java/naevys/NAEVYS/ExcelFiles.java
+++ code/src/main/java/naevys/NAEVYS/ExcelFiles.java
@@ -1,13 +1,22 @@
 package naevys.NAEVYS;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.stream.Stream;
 
 import org.dhatim.fastexcel.Color;
 import org.dhatim.fastexcel.Workbook;
 import org.dhatim.fastexcel.Worksheet;
+import org.dhatim.fastexcel.reader.ReadableWorkbook;
+import org.dhatim.fastexcel.reader.Row;
+import org.dhatim.fastexcel.reader.Sheet;
 
 public class ExcelFiles {
 	public static void main(String[] args) throws IOException {
@@ -22,5 +31,24 @@ public class ExcelFiles {
 		ws.value(0, 4, 1.234);
 		ws.style(0, 0).bold().fillColor(Color.AMETHYST).set();
 		wb.finish();
+	}
+
+	public void convertExcel(String fileDir, String[] inputHeaders, String[] outputHeaders) {
+		try (InputStream is = new FileInputStream(fileDir); ReadableWorkbook wb = new ReadableWorkbook(is)) {
+		    Sheet sheet = wb.getFirstSheet();
+		    try (Stream<Row> rows = sheet.openStream()) {
+		        rows.forEach(r -> {
+		            BigDecimal num = r.getCellAsNumber(0).orElse(null);
+		            String str = r.getCellAsString(1).orElse(null);
+		            LocalDateTime date = r.getCellAsDate(2).orElse(null);
+		        });
+		    }
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
