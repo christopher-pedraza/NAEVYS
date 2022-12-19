40198b00d42b19617d2ef3d96cf32a25d2a79438
Christopher Pedraza <gabalixreborn@gmail.com>
2022-12-19 12:21:15 -0600

    Documentar clase Header

--- code/src/main/java/naevys/NAEVYS/Header.java
+++ code/src/main/java/naevys/NAEVYS/Header.java
@@ -1,26 +1,53 @@
 package naevys.NAEVYS;
 
-// TODO: agregar archivo de Style.txt donde se puedan crear estilos para aplicar a celdas
+// TODO: agregar archivo de Style.txt donde se puedan crear estilos para aplicar a celdas y agregar atributo a header para que cada columna sepa que estilo tiene que aplicar
 
 /**
  * Clase utilizada para guardar los datos de las columnas que seran leidas del
  * archivo fuente al igual que las columnas que se generaran en el archivo de
  * salida
+ * 
+ * @author Christopher Gabriel Pedraza Pohlenz
  */
 public class Header {
+	/**
+	 * Nombre de la columna que se imprimira en el archivo de salida
+	 */
 	private String colName;
+	/**
+	 * Indice donde se imprimira la columna en el archivo de salida
+	 */
 	private int colIndex;
+	/**
+	 * Identificador sobre el tipo de columna de la que se trata (referencia,
+	 * formula, estatico)
+	 */
 	private char id;
+	/**
+	 * Valor (estatico o formula) o referencia (nombre de la columna que se leera
+	 * del archivo de entrada) que se colocara en la columna
+	 */
 	private String value;
+	/**
+	 * Indice de la columna que se leera del archivo de entrada. Toma prioridad el
+	 * nombre de la columna expresado en 'value' en caso de que no corresponda el
+	 * indice
+	 */
 	private int valueIndex;
 
 	/**
-	 * @param colName    nombre de la columna
-	 * @param colIndex   indice donde se deberia encontra la columna con el nombre
-	 *                   colName
-	 * @param id         identificador sobre el tipo de columna de la que se trata
-	 * @param value      valor o referencia que se colocara en la columna
-	 * @param valueIndex indice de la columna de valor en caso de ser una referencia
+	 * @param colName    Nombre de la columna que se imprimira en el archivo de
+	 *                   salida
+	 * @param colIndex   Indice donde se imprimira la columna en el archivo de
+	 *                   salida
+	 * @param id         Identificador sobre el tipo de columna de la que se trata
+	 *                   (referencia, formula, estatico)
+	 * @param value      Valor (estatico o formula) o referencia (nombre de la
+	 *                   columna que se leera del archivo de entrada) que se
+	 *                   colocara en la columna
+	 * @param valueIndex Indice de la columna que se leera del archivo de entrada.
+	 *                   Toma prioridad el nombre de la columna expresado en 'value'
+	 *                   en caso de que no corresponda el indice
 	 */
 	public Header(String colName, int colIndex, char id, String value, int valueIndex) {
 		this.colName = colName;
@@ -29,13 +56,15 @@ public class Header {
 		this.value = value;
 		this.valueIndex = valueIndex - 1;
 	}
-	
+
 	/**
-	 * @param colName    nombre de la columna
-	 * @param colIndex   indice donde se deberia encontra la columna con el nombre
-	 *                   colName
-	 * @param id         identificador sobre el tipo de columna de la que se trata
-	 * @param value      valor o referencia que se colocara en la columna
+	 * @param colName  Nombre de la columna que se imprimira en el archivo de salida
+	 * @param colIndex Indice donde se imprimira la columna en el archivo de salida
+	 * @param id       Identificador sobre el tipo de columna de la que se trata
+	 *                 (referencia, formula, estatico)
+	 * @param value    Valor (estatico o formula) o referencia (nombre de la columna
+	 *                 que se leera del archivo de entrada) que se colocara en la
+	 *                 columna
 	 */
 	public Header(String colName, int colIndex, char id, String value) {
 		this.colName = colName;
@@ -46,49 +75,57 @@ public class Header {
 	}
 
 	/**
-	 * @return the colIndex
+	 * @return {@code colIndex} Indice donde se imprimira la columna en el archivo
+	 *         de salida
 	 */
 	public int getColIndex() {
 		return colIndex;
 	}
 
 	/**
-	 * @return the colName
+	 * @return {@code colName} Nombre de la columna que se imprimira en el archivo
+	 *         de salida
 	 */
 	public String getColName() {
 		return colName;
 	}
 
 	/**
-	 * @return the id
+	 * @return {@code id} Identificador sobre el tipo de columna de la que se trata
+	 *         (referencia, formula, estatico)
 	 */
 	public char getId() {
 		return id;
 	}
 
 	/**
-	 * @return the value
+	 * @param {@code value} Valor (estatico o formula) o referencia (nombre de la
+	 * columna que se leera del archivo de entrada) que se colocara en la columna
 	 */
 	public String getValue() {
 		return value;
 	}
-	
+
 	/**
-	 * @return the valueIndex
+	 * @return {@code ValueIndex} Indice de la columna que se leera del archivo de
+	 *         entrada. Toma prioridad el nombre de la columna expresado en 'value'
+	 *         en caso de que no corresponda el indice
 	 */
 	public int getValueIndex() {
 		return valueIndex;
 	}
 
 	/**
-	 * @param colIndex the colIndex to set
+	 * @param colIndex Indice donde se imprimira la columna en el archivo de salida
 	 */
 	public void setColIndex(int colIndex) {
 		this.colIndex = colIndex - 1;
 	}
-	
+
 	/**
-	 * @param colIndex the colIndex to set
+	 * @param valueIndex Indice de la columna que se leera del archivo de entrada.
+	 *                   Toma prioridad el nombre de la columna expresado en 'value'
+	 *                   en caso de que no corresponda el indice
 	 */
 	public void setValueIndex(int valueIndex) {
 		this.valueIndex = valueIndex - 1;
