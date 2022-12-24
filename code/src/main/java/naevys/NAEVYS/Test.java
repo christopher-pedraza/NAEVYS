a043c7959b210689eeec1f3ed7def1cf05ffb042
Christopher Pedraza <gabalixreborn@gmail.com>
2022-12-23 19:16:28 -0600

    Agregar ejemplo para leer archivo de configuracion

---
+++ code/src/main/java/naevys/NAEVYS/Test.java
@@ -0,0 +1,23 @@
+package naevys.NAEVYS;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Properties prop = new Properties();
+		String fileName = "app.config";
+		try (FileInputStream fis = new FileInputStream(fileName)) {
+		    prop.load(fis);
+		} catch (FileNotFoundException ex) {
+		} catch (IOException ex) {
+		}
+		System.out.println(prop.getProperty("app.name"));
+		System.out.println(prop.getProperty("app.version"));
+
+	}
+
+}
