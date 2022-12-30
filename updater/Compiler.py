75864e0d344d30cb670cb9c0d4345b84d3bfe397
Christopher Pedraza <gabalixreborn@gmail.com>
2022-12-30 16:13:08 -0600

    Agregar color al autoupdater y un script para compilarlo

---
+++ updater/Compiler.py
@@ -0,0 +1,7 @@
+import PyInstaller.__main__
+
+PyInstaller.__main__.run([
+    'NAEVYS_Updater.py',
+    '--onefile',
+    '--icon=res\\icon.ico'
+])
--- updater/NAEVYS_Updater.py
+++ updater/NAEVYS_Updater.py
@@ -1,5 +1,16 @@
+# Autor: Christopher Gabriel Pedraza Pohlenz
+
+# Crear ejecutable del script con la siguiente linea: "pyinstaller --onefile NAEVYS_Updater.py"
+
 # Libreria necesaria para hacer peticiones a paginas web
 import requests
+
+# Colores para el texto
+# Referencia: https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
+BLUE = '\033[94m'
+CYAN = '\033[96m'
+GREEN = '\x1b[1;32;40m'
+NORMAL = '\033[0m'
 
 # Obtener el archivo JSON del ultimo release del repositorio
 response = requests.get("https://api.github.com/repos/christopher-pedraza/NAEVYS/releases/latest").json()
@@ -8,19 +19,19 @@ response = requests.get("https://api.github.com/repos/christopher-pedraza/NAEVYS
 list = response["assets"]
 
 # Especifica al usuario la version que se esta actualizando
-print("~~~ Version '" + response["name"] + "' ~~~\n")
+print(CYAN + "\n\n\t~~~ Version '" + response["name"] + "' ~~~\n" + NORMAL)
 
 # Recorre la lista de los assets
 for asset in list:
 	# Despliega el nombre del archivo
-	print("Downloading '" + asset["name"] + "'", end=" .")
+	print("Downloading '" + BLUE + asset["name"] + NORMAL + "'" + NORMAL, end=" .")
 	# Obtiene el archivo a partir del enlace del asset
 	file_dir = requests.get(asset["browser_download_url"])
 	print(".", end="")
 	# Crea el archivo
 	open(asset["name"], "wb").write(file_dir.content)
 	print(". ", end="")
-	print("Finished.")
+	print(GREEN + "Finished." + NORMAL)
 
 # Espera un input para que no se cierre la terminal
-input("\nPress any key to continue...")
+input(NORMAL + "\nPress <Enter> to continue...")
