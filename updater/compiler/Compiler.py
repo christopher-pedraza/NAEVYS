ead217ebef92281386a7f9ee4de7c8a3313e5ad2
Christopher Pedraza <gabalixreborn@gmail.com>
2022-12-30 16:41:26 -0600

    Agregar src del compilador del updater

---
+++ updater/compiler/Compiler.py
@@ -0,0 +1,10 @@
+# Compilador para crear un ejecutable del updater de NAEVYS
+
+import PyInstaller.__main__
+
+PyInstaller.__main__.run([
+    '..\\NAEVYS_Updater.py',
+    '--onefile',
+    '--icon=..\\res\\icon.ico',
+    '--distpath=..\\'
+])
